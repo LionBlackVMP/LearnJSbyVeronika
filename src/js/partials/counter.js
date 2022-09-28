20383ab26ca92e18683db19f70c770c21af01f93
LionBlackVMP <lionblack1079@gmail.com>
2022-09-28 17:08:42 +0200

    to_do version 1.0

--- src/js/partials/counter.js
+++ src/js/partials/counter.js
@@ -1,81 +1,86 @@
 const isCounterPage = document.querySelector(".counter-page");
 if (isCounterPage) {
-}
+  let numberFirstLine = [];
+  let numberSecondLine = [];
 
-let numberFirstLine = [];
-let numberSecondLine = [];
-
-let buttonAdditionFirstLine = document.querySelector(
-  ".buttonAdditionFirstLine"
-);
-let buttonSubtractionFirstLine = document.querySelector(
-  ".buttonSubtractionFirstLine"
-);
-let firstResult = JSON.parse(localStorage.getItem("numberFirstLine") || 0);
-buttonAdditionFirstLine.addEventListener("click", function () {
-  let result = ++firstResult;
-
-  numberFirstLine.splice(0, 1, result);
-  localStorage.setItem("numberFirstLine", JSON.stringify(numberFirstLine));
-  document.getElementById("countFirst").innerHTML = result;
-});
-
-buttonSubtractionFirstLine.addEventListener("click", function () {
-  let result = --firstResult;
-
-  numberFirstLine.splice(0, 1, result);
-  localStorage.setItem("numberFirstLine", JSON.stringify(numberFirstLine));
-  document.getElementById("countFirst").innerHTML = result;
-});
-
-let buttonAdditionSecondLine = document.querySelector(
-  ".buttonAdditionSecondLine"
-);
-let buttonSubtractionSecondLine = document.querySelector(
-  ".buttonSubtractionSecondLine"
-);
-
-let secondResult = JSON.parse(localStorage.getItem("numberSecondLine") || 0);
-buttonAdditionSecondLine.addEventListener("click", function () {
-  setTimeout(function () {
-    let result = ++secondResult;
-
-    document.getElementById("countSecond").innerHTML = result;
-    numberSecondLine.splice(0, 1, result);
-    localStorage.setItem("numberSecondLine", JSON.stringify(numberSecondLine));
-  }, 1000);
-});
-
-buttonSubtractionSecondLine.addEventListener("click", function () {
-  setTimeout(function () {
-    let result = --secondResult;
-
-    document.getElementById("countSecond").innerHTML = result;
-    numberSecondLine.splice(0, 1, result);
-    localStorage.setItem("numberSecondLine", JSON.stringify(numberSecondLine));
-  }, 1000);
-});
-
-let reset = document.querySelector(".button_reset");
-
-reset.addEventListener("click", function () {
-  localStorage.clear();
-  firstResult = 0;
-  secondResult = 0;
-  return (
-    (document.getElementById("countSecond").innerHTML = 0),
-    (document.getElementById("countFirst").innerHTML = 0)
+  let buttonAdditionFirstLine = document.querySelector(
+    ".buttonAdditionFirstLine"
   );
-});
-
-if (JSON.parse(localStorage.getItem("numberFirstLine"))) {
-  document.getElementById("countFirst").innerHTML = JSON.parse(
-    localStorage.getItem("numberFirstLine")
+  let buttonSubtractionFirstLine = document.querySelector(
+    ".buttonSubtractionFirstLine"
   );
-}
+  let firstResult = JSON.parse(localStorage.getItem("numberFirstLine") || 0);
+  buttonAdditionFirstLine.addEventListener("click", function () {
+    let result = ++firstResult;
 
-if (JSON.parse(localStorage.getItem("numberSecondLine"))) {
-  document.getElementById("countSecond").innerHTML = JSON.parse(
-    localStorage.getItem("numberSecondLine")
+    numberFirstLine.splice(0, 1, result);
+    localStorage.setItem("numberFirstLine", JSON.stringify(numberFirstLine));
+    document.getElementById("countFirst").innerHTML = result;
+  });
+
+  buttonSubtractionFirstLine.addEventListener("click", function () {
+    let result = --firstResult;
+
+    numberFirstLine.splice(0, 1, result);
+    localStorage.setItem("numberFirstLine", JSON.stringify(numberFirstLine));
+    document.getElementById("countFirst").innerHTML = result;
+  });
+
+  let buttonAdditionSecondLine = document.querySelector(
+    ".buttonAdditionSecondLine"
   );
+  let buttonSubtractionSecondLine = document.querySelector(
+    ".buttonSubtractionSecondLine"
+  );
+
+  let secondResult = JSON.parse(localStorage.getItem("numberSecondLine") || 0);
+  buttonAdditionSecondLine.addEventListener("click", function () {
+    setTimeout(function () {
+      let result = ++secondResult;
+
+      document.getElementById("countSecond").innerHTML = result;
+      numberSecondLine.splice(0, 1, result);
+      localStorage.setItem(
+        "numberSecondLine",
+        JSON.stringify(numberSecondLine)
+      );
+    }, 1000);
+  });
+
+  buttonSubtractionSecondLine.addEventListener("click", function () {
+    setTimeout(function () {
+      let result = --secondResult;
+
+      document.getElementById("countSecond").innerHTML = result;
+      numberSecondLine.splice(0, 1, result);
+      localStorage.setItem(
+        "numberSecondLine",
+        JSON.stringify(numberSecondLine)
+      );
+    }, 1000);
+  });
+
+  let reset = document.querySelector(".button_reset");
+
+  reset.addEventListener("click", function () {
+    localStorage.clear();
+    firstResult = 0;
+    secondResult = 0;
+    return (
+      (document.getElementById("countSecond").innerHTML = 0),
+      (document.getElementById("countFirst").innerHTML = 0)
+    );
+  });
+
+  if (JSON.parse(localStorage.getItem("numberFirstLine"))) {
+    document.getElementById("countFirst").innerHTML = JSON.parse(
+      localStorage.getItem("numberFirstLine")
+    );
+  }
+
+  if (JSON.parse(localStorage.getItem("numberSecondLine"))) {
+    document.getElementById("countSecond").innerHTML = JSON.parse(
+      localStorage.getItem("numberSecondLine")
+    );
+  }
 }
--- src/js/partials/todo.js
+++ src/js/partials/todo.js
@@ -8,5 +8,58 @@ const isToDoPage = document.querySelector(".to-do-page");
 // let v = undefined;
 
 if (isToDoPage) {
-  alert("true");
+  let taskArrow = [];
+
+  let taskAddForm = document.querySelector("#taskAddForm");
+  let taskIndex = JSON.parse(localStorage.getItem("taskIndex"));
+  function search(event) {
+    event.preventDefault();
+    let taskBar = document.querySelector(".task_bar");
+    let li = document.createElement("li");
+    let inputTask = document.getElementById("task");
+
+    taskBar.appendChild(li);
+    // при обновлении страницы, массив обнуляется и нихера не работает как надо
+    if (taskIndex) {
+      let arrayToDo = JSON.parse(localStorage.getItem("taskIndex"));
+
+      arrayToDo.push(inputTask.value);
+
+      localStorage.setItem("taskIndex", JSON.stringify(arrayToDo));
+      let taskIndex = JSON.parse(localStorage.getItem("taskIndex"));
+      console.log(arrayToDo);
+      let lastElem = taskIndex.slice(-1);
+      li.className = "taskList";
+      li.innerHTML = lastElem;
+    } else {
+      taskArrow.push(inputTask.value);
+      localStorage.setItem("taskIndex", JSON.stringify(taskArrow));
+
+      let taskIndex = JSON.parse(localStorage.getItem("taskIndex"));
+
+      let lastElem = taskIndex.slice(-1);
+      li.className = "taskList";
+      li.innerHTML = lastElem;
+    }
+
+    console.log(taskArrow);
+  }
+
+  taskAddForm.addEventListener("submit", search);
+
+  window.onload = function () {
+    if (taskIndex) {
+      let taskBar = document.querySelector(".task_bar");
+
+      // li.innerHTML = localStorage.taskIndex;
+
+      taskIndex.forEach((item, index, array) => {
+        let li = document.createElement("li");
+        li.className = "taskList";
+        taskBar.appendChild(li);
+        li.innerHTML = item;
+        console.log(`${item} имеет позицию ${index} в ${array}`);
+      });
+    }
+  };
 }
